40733f64cf7347346b9311f193ab6e796a05357f
TheDoc-JR <darlingtonablorh7@gmail.com>
2022-07-01 15:24:43 +0200

    Update Clinic files.py

--- Clinic files.py
+++ Clinic files.py
@@ -9,6 +9,17 @@ class Patient:
         self.id = id
 
 
+def wrong_option():
+    option = input()
+    while option not in 'AaBbCc':
+        print('Type a valid option')
+        option = input()
+
+
+
+
+
+
 
 def new_patient():
     name = input('Please enter your name: ')
@@ -23,9 +34,31 @@ def print_record():
     if input_id in Data.record:
         print('Last name: {}\nName: {}\nID number: {}'.format(Data.record[input_id][0],\
                                                               Data.record[input_id][1], Data.record[input_id][2]))
+        select = input('Would you like to:\na) Try another ID number\nb) Go to main menu\nc) Exit\n')
+        if select in 'Aa':
+            print_record()
+        elif select in 'Bb':
+            menu()
+        elif select in 'Cc':
+            print('Thank you for your time, hope we have helped!')
+
+
+
     else:
-        print('The ID number given is not in our current database')
-        pass
+        while input_id not in Data.record:
+            print('The ID number given is not in our current database')
+            select2 = input('Would you like to:\na) Try another ID number\nb) Go to main menu\nc) Exit\n')
+            while select2 not in 'Cc':
+                if select2 in 'Aa':
+                    print_record()
+                elif select2 in 'Bb':
+                    menu()
+                elif select2 in 'Cc':
+                    print('Thank you for your time, hope we have helped!')
+                    break
+            else:
+                print('Thank you for your time, hope we have helped!')
+                break
 
 
 
@@ -33,22 +66,21 @@ def menu():
     option = input('Please choose one of this options typing the corresponding letter:\n\
 a) Add new patient\nb) Show patient info\nc) Exit\n')
 
-    while option not in 'AaBbCc':
-        print('Type a valid option')
-        option = input('Please choose one of this options typing the corresponding letter:\n\
-a) Add new patient\nb) Show patient info\nc) Exit\n')
 
-    while option == 'a' or option == 'A':
+    while option in 'Aa':
         new_patient()
         print('Patient successfully added!')
         option = input('Please choose one of this options typing the corresponding letter:\n\
 a) Add new patient\nb) Show patient info\nc) Exit\n')
 
-    if option == 'b' or option == 'B':
+
+    if option in 'Bb':
         print_record()
 
-    if option == 'c' or option == 'C':
+
+    if option in 'Cc':
         print('Thank you for your time, hope we have helped!')
+
 
 
 # Main code
