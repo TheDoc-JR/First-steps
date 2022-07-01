d485e361d99bd30dce36fdca4a3feb7d38c5b044
TheDoc-JR <darlingtonablorh7@gmail.com>
2022-07-01 19:18:26 +0200

    Update Clinic files.py

--- Clinic files.py
+++ Clinic files.py
@@ -3,10 +3,11 @@ class Data:
 
 
 class Patient:
-    def __init__(self, name, last_name, id):
+    def __init__(self, name, last_name, id, gender):
         self.name = name
         self.last_name = last_name
         self.id = id
+        self.gender = gender
 
 
 def wrong_option():
@@ -25,15 +26,26 @@ def new_patient():
     name = input('Please enter your name: ')
     last_name = input('Please enter your last name: ')
     id = input('Please enter your ID number: ')
-    new_p = Patient(name, last_name, id)
-    Data.record[id] = [new_p.last_name, new_p.name, new_p.id]
+    gender = input('Please type Male(M) or Female(F): ')
+    while gender not in 'MmFf':
+        print('{} is not a valid option.'.format(gender))
+        gender = input('Please type Male(M) or Female(F): ')
+    if gender in 'Mm':
+        gender = 'male'
+    elif gender in 'Ff':
+        gender = 'female'
+    new_p = Patient(name, last_name, id, gender)
+    Data.record[id] = [new_p.last_name, new_p.name, new_p.id, gender]
     return new_p
 
 def print_record():
     input_id = input('Please enter your ID number to visualize your info: ')
     if input_id in Data.record:
-        print('Last name: {}\nName: {}\nID number: {}'.format(Data.record[input_id][0],\
-                                                              Data.record[input_id][1], Data.record[input_id][2]))
+        for i in range(len(Data.record[input_id])):
+            data = ''
+            data += Data.record[input_id][i]
+            print(data)
+
         select = input('Would you like to:\na) Try another ID number\nb) Go to main menu\nc) Exit\n')
         if select in 'Aa':
             print_record()
@@ -41,8 +53,6 @@ def print_record():
             menu()
         elif select in 'Cc':
             print('Thank you for your time, hope we have helped!')
-
-
 
     else:
         while input_id not in Data.record:
